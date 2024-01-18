0dafdc12775be8115bc9ca6447553d8d562c22e8
juanl2004 <juanluis.barrionuevo@iesnervion.es>
2024-01-18 17:29:48 +0100

    Primer commit

---
+++ src/comandos_M_J/Ejercicio1.java
@@ -0,0 +1,34 @@
+package comandos_M_J;
+
+import java.util.Scanner;
+
+public class Ejercicio1 {
+
+	public static void main(String[] args) {
+
+		int num1;
+
+		int num2;
+
+		int suma;
+
+		Scanner sc = new Scanner(System.in);
+
+		System.out.print("Introduce el primer número --> ");
+		num1 = sc.nextInt();
+
+		System.out.print("Introduce el segundo número --> ");
+		num2 = sc.nextInt();
+
+		suma = num1 + num2;
+
+		if (suma % 2 == 0) {
+			System.out.println("La suma de " + num1 + " + " + num2 + " = " + suma + " --> La solución es ¡PAR!");
+		} else {
+			System.out.println("La suma de " + num1 + " + " + num2 + " = " + suma + " --> La solución es ¡IMPAR!");
+		}
+
+		sc.close();
+	}
+
+}
